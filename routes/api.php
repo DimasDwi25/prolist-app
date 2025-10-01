dc30997576963315ae81d8fb5c7b027f507494c4
DimasDwi25 <dwid4503@gmail.com>
2025-10-01 13:28:52 +0700

    update route

--- routes/api.php
+++ routes/api.php
@@ -45,6 +45,7 @@ use Illuminate\Support\Facades\Route;
 
 // Login & CSRF cookie
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/refresh', [AuthController::class, 'refresh']);
 Route::post('/logout', [AuthController::class, 'logout']);
 Route::get('/user', [AuthController::class, 'me']);
 
